21eae8d4f6575776fbce084a2ac75acfd166a5ec
Michael Tolsma <mtolsma19@gmail.com>
2024-06-24 21:46:30 +0200

    Reorder CV

--- src/routes/cv/+page.svelte
+++ src/routes/cv/+page.svelte
@@ -14,25 +14,24 @@
           <span class="text-xl sm:text-2xl lg:text-3xl text-yellow-500 fa fa-school mr-1"></span>
           <span class="text-xl sm:text-2xl lg:text-3xl font-bold">Education</span>
         </div>
-        
         <div class="mb-4">
-          <span class="font-bold">High School</span>
-          <p class="text-yellow-500">2003 - 2007</p>
-          <p>Woodridge College</p>
+          <span class="font-bold">Bachelor of Science: Computing</span>
+          <p class="text-yellow-500">2017 - 2021</p>
+          <p>University of South Africa</p>
+          <p class="italic">Graduated Cum Laude</p>
         </div>
-    
+
         <div class="mb-4">
           <span class="font-bold">National Diploma: Engineering: Civil</span>
           <p class="text-yellow-500">2010 - 2012</p>
           <p>Nelson Mandela University</p>
           <p class="italic">Graduated Cum Laude</p>
         </div>
-    
+
         <div class="mb-4">
-          <span class="font-bold">Bachelor of Science: Computing</span>
-          <p class="text-yellow-500">2017 - 2021</p>
-          <p>University of South Africa</p>
-          <p class="italic">Graduated Cum Laude</p>
+          <span class="font-bold">High School</span>
+          <p class="text-yellow-500">2003 - 2007</p>
+          <p>Woodridge College</p>
         </div>
       </div>
 
@@ -43,13 +42,19 @@
           <span class="text-xl sm:text-2xl lg:text-3xl text-yellow-500 fa fa-briefcase mr-1"></span>
           <span class="text-xl sm:text-2xl lg:text-3xl font-bold">Experience</span>
         </div>
-        
+
         <div class="mb-4">
-          <span class="font-bold">Scuba Diving Instructor at Pro Dive</span>
-          <p class="text-yellow-500">Jan 2010 - Jan 2015</p>
-          <p>Lead clients on shore and boat-based dives; taught new and experienced people how to scuba dive; handled bookings, phone calls, and scheduling.</p>
+          <span class="font-bold">Junior Software Developer at Teach Me 2</span>
+          <p class="text-yellow-500">Sep 2021 - Present</p>
+          <p>Maintaining, upgrading, and enhancing the company website, which serves as a critical tool for staff to capture customer data, and customers to view important information. Ensure seamless functionality and user experience utilizing Ruby on Rails for backend development and JS/React for frontend development.</p>
         </div>
-        
+
+        <div class="mb-4">
+          <span class="font-bold">Junior Mobile/Flutter Android Developer Internship at Melon Stall</span>
+          <p class="text-yellow-500">Nov 2019 - Jan 2021</p>
+          <p>Going over feature requirements and making sure they're understood; using git/Github to manage changes; developing features against emulators and real devices; creating prototypes for more advanced features; handing over of APKs and deploying into Google Play Store.</p>
+        </div>
+
         <div class="mb-4">
           <span class="font-bold">Ocean Blue Adventures</span>
           <p class="text-yellow-500">Jan 2015 - Dec 2018</p>
@@ -58,15 +63,9 @@
         </div>
         
         <div class="mb-4">
-          <span class="font-bold">Junior Mobile/Flutter Android Developer Internship at Melon Stall</span>
-          <p class="text-yellow-500">Nov 2019 - Jan 2021</p>
-          <p>Going over feature requirements and making sure they're understood; using git/Github to manage changes; developing features against emulators and real devices; creating prototypes for more advanced features; handing over of APKs and deploying into Google Play Store.</p>
-        </div>
-    
-        <div class="mb-4">
-          <span class="font-bold">Junior Software Developer at Teach Me 2</span>
-          <p class="text-yellow-500">Sep 2021 - Present</p>
-          <p>Maintaining, upgrading, and enhancing the company website, which serves as a critical tool for staff to capture customer data, and customers to view important information. Ensure seamless functionality and user experience utilizing Ruby on Rails for backend development and JS/React for frontend development.</p>
+          <span class="font-bold">Scuba Diving Instructor at Pro Dive</span>
+          <p class="text-yellow-500">Jan 2010 - Jan 2015</p>
+          <p>Lead clients on shore and boat-based dives; taught new and experienced people how to scuba dive; handled bookings, phone calls, and scheduling.</p>
         </div>
       </div>
 
